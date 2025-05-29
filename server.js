fdda16887bafd2de1fae761297f9c1fc84fddfaf
Sambatra Gaël <104251392+sambatraga@users.noreply.github.com>
2025-05-29 10:38:33 +0000

    Update server.js

--- server.js
+++ server.js
@@ -104,6 +104,6 @@ app.use(errorMiddleware)
 app.listen(port, "0.0.0.0");
 console.log('Serveur démarré sur http://localhost:' + port);
 
-console.log(-----"Deployementtttt Okkkkk")
+console.log("-----------Deployementtttt Okkkkk")
 
 module.exports = app;
